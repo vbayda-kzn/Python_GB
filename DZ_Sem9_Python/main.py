bf4fac02a77fddfad6547a824c771141665da3ed
Vladimir_B <vladimir.baida@gmail.com>
2022-10-05 22:44:15 +0300

    ДЗ по семинару 9 по Python

---
+++ DZ_Sem9_Python/main.py
@@ -0,0 +1,36 @@
+# Напишите бота, удаляющего из текста все слова, содержащие "абв". (текст вводит пользователь)
+# Название бота - Learn_Python_Bot,    адрес бота - @vb_python_bot.
+
+from telegram import Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+bot = Bot(token='5786244623:AAFP9jAt8daFXk-5ySYgLSyhlAWeNm1pEMk')
+updater = Updater(token='5786244623:AAFP9jAt8daFXk-5ySYgLSyhlAWeNm1pEMk')
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    context.bot.send_message(update.effective_chat.id, 'Привет, чувак!')
+    context.bot.send_message(update.effective_chat.id, 'Напиши мне что-нибудь, а я попытаюсь удалить слова, содержащие строку "абв"')
+    
+def find_and_cut(update, context):
+    
+    phrase = update.message.text.split()
+    if phrase[0][0] == "/":
+        context.bot.send_message(update.effective_chat.id, 'Приятель, я таких команд не знаю.')
+    else:
+        context.bot.send_message(update.effective_chat.id, 'Ок. Ща, пять сек!')
+        for word in phrase:
+            if "абв" in word.lower():
+                phrase.remove(word)
+        context.bot.send_message(update.effective_chat.id, " ".join(phrase))
+
+
+start_handler = CommandHandler('start', start)
+message_handler = MessageHandler(Filters.text, find_and_cut)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(message_handler)
+
+updater.start_polling()
+updater.idle()
